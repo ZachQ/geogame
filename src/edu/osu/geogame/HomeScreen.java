fc4dbe8f83e08065266758c2bccb40f5479371c8
zaq831zaq <zaq831zaq@945dee0b-d511-2e01-03d3-a46ee05a2456>
2012-04-04 21:19:41 +0000

    Made the splash screen come up first, then move onto the "HomeScreen" after 6 seconds. Right now the buttons on the home screen do not work, but will soon

---
+++ src/edu/osu/geogame/HomeScreen.java
@@ -0,0 +1,14 @@
+package edu.osu.geogame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        
+    }
+}
---
+++ src/edu/osu/geogame/Splash.java
@@ -0,0 +1,49 @@
+package edu.osu.geogame;
+
+
+import edu.osu.geogame.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * This is basically the "splash screen" that comes up when the app
+ * is opened to display GeoGame logo, Geo dept and Ohio State University
+ * @author Zachary Quinn
+ */
+public class Splash extends Activity {
+    /** Called when the activity is first created. */
+	
+	int counter;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+		final int sleepTime = 6000;
+		// timer to sleep for 5 seconds
+		Thread timer = new Thread() {
+			// wait for 5 seconds, then proceed to the next screen to login
+			public void run() {
+				try {
+					sleep(sleepTime);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally{
+					// Proceed to the next "home" screen
+					Intent openStartingPoint = new Intent("edu.osu.geogame.HOMESCREEN");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+	
+	@Override
+	protected void onPause() {
+		// this will "pause" the activity
+		super.onPause();
+		finish();
+	}
+
+}
