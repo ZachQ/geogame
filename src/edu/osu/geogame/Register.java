60c161494d7dbb1508802cde3c2c9d3277129359
zaq831zaq <zaq831zaq@945dee0b-d511-2e01-03d3-a46ee05a2456>
2012-04-07 17:12:55 +0000

    chaged "register.java" to go back to login screen once they click register button

--- src/edu/osu/geogame/Register.java
+++ src/edu/osu/geogame/Register.java
@@ -1,16 +1,35 @@
 package edu.osu.geogame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class Register extends Activity {
 
+	Button register;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
+		
+		// Get register reference
+		register = (Button) findViewById(R.id.buttonRegister2);
+		
+		// Add ActionListeners to buttons
+		register.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO:: Try to register
+				
+				Intent myIntent = new Intent(v.getContext(), Login.class);
+                startActivityForResult(myIntent, 0);
+			}
+		});
 	}
 	
 	/*
