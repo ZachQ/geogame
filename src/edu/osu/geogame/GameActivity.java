c893e4b0d5852cad2badf7ea6e80f1d5736ea25a
d.fischer874@gmail.com <d.fischer874@gmail.com@945dee0b-d511-2e01-03d3-a46ee05a2456>
2012-05-06 21:21:58 +0000

    added some icons and some more text to the map tab

--- src/edu/osu/geogame/GameActivity.java
+++ src/edu/osu/geogame/GameActivity.java
@@ -22,27 +22,33 @@ public class GameActivity extends TabActivity {
 		
 		//Add Overview Tab
 		tabSpec = 	tabs.newTabSpec("Home")
-					.setIndicator("Home")
+					.setIndicator("Home", getResources().getDrawable(R.drawable.home))
 					.setContent( new Intent( this, HomeTabActivity.class) );
 		tabs.addTab(tabSpec);
 		
 		//Add Market Tab
 		tabSpec = 	tabs.newTabSpec("Market")
-					.setIndicator("Market")
+					.setIndicator("Market", getResources().getDrawable(R.drawable.market))
 					.setContent( new Intent( this, MarketTabActivity.class) );
 		tabs.addTab(tabSpec);
 		
 		//Add Map Tab
 		tabSpec = 	tabs.newTabSpec("Map")
-					.setIndicator("Map")
+					.setIndicator("Map", getResources().getDrawable(R.drawable.map))
 					.setContent( new Intent( this, MapTabActivity.class ) );
 		tabs.addTab(tabSpec);
 		
 		//Add Scoreboard Tab
-		tabSpec = 	tabs.newTabSpec("Scoreboard")
-					.setIndicator("Scoreboard")
+		tabSpec = 	tabs.newTabSpec("Score")
+					.setIndicator("Score", getResources().getDrawable(R.drawable.trophy))
 					.setContent( new Intent( this, ScoreboardTabActivity.class ) );
 		tabs.addTab(tabSpec);
+		
+		tabSpec = 	tabs.newTabSpec("Forum")
+				.setIndicator("Forum", getResources().getDrawable(R.drawable.forum))
+				.setContent( new Intent( this, ForumTabActivity.class ) );
+	tabs.addTab(tabSpec);
+	
 		
 		
 	}
