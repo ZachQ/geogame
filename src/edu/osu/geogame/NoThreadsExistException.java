c8bf56c8484c33b1b4b5849ab9879a8992c679c6
d.fischer874@gmail.com <d.fischer874@gmail.com@945dee0b-d511-2e01-03d3-a46ee05a2456>
2012-05-07 19:54:28 +0000

    Started the forum

---
+++ src/edu/osu/geogame/NoThreadsExistException.java
@@ -0,0 +1,13 @@
+package edu.osu.geogame;
+
+public class NoThreadsExistException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public NoThreadsExistException() {
+		super();
+	}
+}
